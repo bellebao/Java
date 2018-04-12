ac97806f3292ce28ae8400b6a5a9a44599fcd5dd
belle <belle.bao@gmail.com>
2018-04-12 10:15:46 +0800

    first practice for java

---
+++ FreshJuiceTest.java
@@ -0,0 +1,12 @@
+class FreshJuice{
+	enum FreshJuiceSize{small, medium, large}
+	FreshJuiceSize size;
+}
+
+public class FreshJuiceTest{
+	public static void main(String args[]){
+		FreshJuice juice = new FreshJuice();
+		juice.size = FreshJuice.FreshJuiceSize.medium;
+		System.out.println(juice.size);
+	}	
+}
